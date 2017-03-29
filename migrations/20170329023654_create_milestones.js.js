bfee4193a6d20d4b4e7c889776cf5f6b8c47ba38
Sumeet Minhas <sumeetm273@gmail.com>
2017-03-29 03:10:42 +0000

    adding id PK

---
+++ migrations/20170329023654_create_milestones.js.js
@@ -0,0 +1,15 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('milestones', (table) => {
+      table.increments('id').primary().unique();
+      table.string('description');
+      table.date('date_achieved');
+    })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+  knex.schema.dropTable('milestones')
+  ]);
+};
